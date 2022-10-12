a2b5763f9c418d0cd5e2497132232e9d5797282e
Bao Minh <haphanabominh9674@gmail.com>
2022-10-12 23:00:07 +0700

    oke

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://chatapprealtime.vercel.app"],
+    origin: ["https://chatapprealtime.vercel.app", "http://localhost:3000"],
     Credential: true,
   },
 });
